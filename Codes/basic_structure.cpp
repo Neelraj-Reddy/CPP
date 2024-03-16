191f0d07f1bd6cd751fd6004d0ff64ca765f289d
Neelraj-Reddy <neelrajreddy1694@gmail.com>
2024-03-16 12:13:10 +0530

    initial

---
+++ Codes/basic_structure.cpp
@@ -0,0 +1,21 @@
+#include<iostream> // Preprocessor directive
+
+// Preprocessor directive contains instructions and predifined constants that can be used in the program. 
+// These directives are processed first independent of their location in the code.
+
+int main(){ //function declarator
+    //function body
+    std::cout<<"Hello world";
+    return 0;
+}
+
+
+// The above code is source code which is passed to compiler to be translated to machine code.
+// << this left shift operator used above is called as insertion operator or put-to operator.
+// >> this right shift operator to get input in cin>> is know as extraction operator
+// more than one output can be dispalyed in a single line which is know as cascaded output operations in cpp
+//  eg : cout<< "hello " << variable << " nice to mee ya";
+// cout<<endl; serves the same purpose as cout<<'\n' and it is known as manipulator.
+// cascaded input operation is possible in cpp.
+
+
