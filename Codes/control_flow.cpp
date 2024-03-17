54c53cc7cc07cf016e975bcdbcd636ee6db15f52
Neelraj-Reddy <neelrajreddy1694@gmail.com>
2024-03-17 13:59:56 +0530

    initial

---
+++ Codes/control_flow.cpp
@@ -0,0 +1,38 @@
+// expression1 ? expression2 : expression3;                    --------------> conditional operator
+
+// typedef datatype identifier                            -----------> typedef long int lint; ---------------> now lint is of type long int
+
+
+// #define add (a, b) (a+b)  // macro function -----------> #define name (parameters) function body
+// macro functions can take expression such as add(++a) but remember the expression is not evaluvated at the time of call and it is only evaluvated at the time of their usage.
+
+// we call size(short, long) and sign (signed, unsigned) as qualifiers rather than data types.
+
+// switch and goto are branching statements
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------->
+//                                                                Problems
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+// Program to print reverse of an number
+
+
+#include<iostream>
+
+int main(){
+    int num;
+    std::cout<< "Enter the num : ";
+    std::cin>> num;
+    
+    
+}
+
+
+
+
+
+
+
+
