14a382c751754a742d88f2ca1f9bf1f0f62ab1c0
Neelraj-Reddy <neelrajreddy1694@gmail.com>
2024-03-17 18:19:52 +0530

    update

--- Practiced Problems/sum_of_square_of_1st_n.cpp
+++ Practiced Problems/sum_of_square_of_1st_n.cpp
@@ -11,9 +11,18 @@ int main(){
     std::cout<<"Enter the number : ";
     std::cin>>num;
 
+    // Brute force :
+
     for(int i=1; i<num ;i++){
         sum+=pow(i,2);
     }
+
+    // Formula :
+    //  sum = ((n)*(n+1)) / 2                  -----------------> sum of natural numbers
+    //  sum = (n * (n+1) * (2*n + 1)) / 6      -----------------> sum of squares
+    //  sum = (((n)*(n+1)) / 2) ^ 2            -----------------> sum of cubes
+
+    sum = (num * (num +1) * (2*num + 1)) / 6 ;
 
     std::cout<<"sum of squares upto "<<num<<" is "<< sum;
 }
