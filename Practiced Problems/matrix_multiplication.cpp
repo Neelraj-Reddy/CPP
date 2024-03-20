04ec9a25e694119e60a2ce76d1b500f63d09f1e6
Neelraj-Reddy <neelrajreddy1694@gmail.com>
2024-03-20 21:53:20 +0530

    initial

---
+++ Practiced Problems/matrix_multiplication.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row1,column1,row2,column2;
+    cout<<"Enter rows and columns of matrix 1 :";
+    cin>>row1>>column1;
+    cout<<"Enter rows and coloumns of matrix 2 :";
+    cin>>row2>>column2;
+
+    if(column1==row2){
+
+        int matrix1[row1][column1],matrix2[row2][column2];
+
+        cout<<"matrix 1 elements : "<<endl;
+        for(int i=0;i<row1;i++){
+            for(int j=0;j<column1;j++){
+                cin>>matrix1[i][j];
+            }
+        }
+
+        cout<<"matrix 2 elements : "<<endl;
+        for(int i=0;i<row2;i++){
+            for(int j=0;j<column2;j++){
+                cin>>matrix2[i][j];
+            }
+        }
+
+        
+
+
+    }
+    else{
+        cout<<"Not matching dimensions. ";
+    }
+}
